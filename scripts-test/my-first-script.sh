21d88e85c8dccc8b899d75d021e695c30eca6152
Fredy Zarate <fzarate@noticiasnet.mx>
2022-10-28 11:23:44 -0500

    Remplazo de comando cat por echo

--- scripts-test/my-first-script.sh
+++ scripts-test/my-first-script.sh
@@ -4,13 +4,15 @@ echo Este es mi primer script
 FILE=~/fredy.sh
  if [ -f "$FILE" ]; then
  echo "$FILE ya existe"
+ #cat >> ~/fredy.sh
+ #actuaizacion de archivo
  else
  echo "$FILE no existe"
  echo Voy a crear el archivo .sh
- cat > ~/fredy.sh
- Archivo creado correctamente
- ctrl+D
- echo El archivo fue creado correctamente
+ #cat > ~/fredy.sh
+ #Archivo creado correctamente
+ #ctrl+d
+ echo "El archivo fue creado correctamente" >> ~/fredy.sh
  fi
  sudo crontab -e
- */15 * * * * bin/sh ~/fredy.sh &> ~/log_script.log
+ */1 * * * * bin/sh ~/fredy.sh &> ~/log_script.log
