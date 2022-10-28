279b7f54655f3bee492bf9f0eba46dec9b228041
Fredy Zarate <fzarate@noticiasnet.mx>
2022-10-28 10:24:11 -0500

    Modificacion de la ruta home y se agrega coronetab

--- scripts-test/my-first-script.sh
+++ scripts-test/my-first-script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo Este es mi primer script
 
-FILE=/Users/fzarate/fredy.sh
+FILE=~/fredy.sh
  if [ -f "$FILE" ]; then
- echo "$FILE existe"
+ echo "$FILE ya existe"
  else
  echo "$FILE no existe"
  echo Voy a crear el archivo .sh
+ cat > ~/fredy.sh
+ Archivo creado correctamente
+ ctrl+D
+ echo El archivo fue creado correctamente
  fi
+ sudo crontab -e
+ */15 * * * * bin/sh ~/fredy.sh &> ~/log_script.log
