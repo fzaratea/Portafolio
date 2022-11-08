2b0685bc4bb55dcec1ee4def865ae87686a6c4f0
Fredy Zarate Arango <fredyzaratearango@MacBook-Pro-de-Fredy.local>
2022-11-08 15:01:03 -0600

    Clases.js actualizada

--- practicas/repaso_js/clases.js
+++ practicas/repaso_js/clases.js
@@ -10,9 +10,16 @@ class coche {
     }
 
     reducirVelocidad(){
-        this.velocidad += 1;
+        this.velocidad -= 1;
     }
 
 }
 
-var coche1 = new coche('BMW',200, 2017);
+var coche1 = new coche('BMW',280, 2017);
+var coche2 = new coche('Audi', 260, 2018);
+var coche3 = new coche('Mercedes', 230, 2016);
+var coche4 = new coche('Reault', 180, 2008);
+
+document.write("<h1>Velocidad: "+coche1.velocidad+"</h1>");
+coche1.aumentarVelocidad();
+coche1.aumentarVelocidad();
