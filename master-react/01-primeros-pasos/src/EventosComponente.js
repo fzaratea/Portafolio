4c5be9a75bd8d68f65da6156e132254191133ea5
Fredy Zarate Arango <fredyzaratearango@MacBook-Pro-de-Fredy.local>
2022-11-15 11:34:16 -0600

    eventos: mouse enter y leave, focus y blur

--- master-react/01-primeros-pasos/src/EventosComponente.js
+++ master-react/01-primeros-pasos/src/EventosComponente.js
@@ -9,6 +9,18 @@ export const EventosComponente = () => {
     alert("Has dado doble click al boton !!");
   }
 
+  const hasentrado = (e, accion) => {
+    alert(`Has ${accion} la caja con el mouse!!`);
+  }
+
+  const estasdentro = e => {
+    console.log("Estas dentro del input, ingresa tu nombre!!");
+  }
+
+  const estasfuera = e => {
+    console.log("Estas fuera del input, Hasta la proxima!!");
+  }
+
   return (
     <div>
         <h1>Eventos en React</h1>
@@ -21,6 +33,22 @@ export const EventosComponente = () => {
         <p>
           {/*Evento doble click*/}
           <button onDoubleClick={ hasdadodobleclick }>Dame doble click!</button>
+        </p>
+
+        <div id="caja"
+          onMouseEnter={ e => hasentrado(e, "entrado a") }
+          onMouseLeave={ e => hasentrado(e, "salido de") }
+        >
+            {/*Evento onMouseEnter onMouseLeave*/}
+            Pasa por encima !!
+        </div>
+
+            {/*Evento focus y blur */}
+        <p>
+            <input type="text" 
+                    onFocus={ estasdentro } 
+                    onBlur={ estasfuera }
+                    placeholder="Introduce tu nombre..."/>
         </p>
     </div>
   )
