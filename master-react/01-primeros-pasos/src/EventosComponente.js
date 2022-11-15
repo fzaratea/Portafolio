c8338f55af3708190e62f53322316594ee68f274
Fredy Zarate Arango <fredyzaratearango@MacBook-Pro-de-Fredy.local>
2022-11-15 10:32:22 -0600

    Evento click y doble click

--- master-react/01-primeros-pasos/src/EventosComponente.js
+++ master-react/01-primeros-pasos/src/EventosComponente.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
 export const EventosComponente = () => {
+  const hasdadoclick = (e) => {
+    alert("Has dado click al boton !!");
+  }
+
+  function hasdadodobleclick(e){
+    alert("Has dado doble click al boton !!");
+  }
+
   return (
     <div>
         <h1>Eventos en React</h1>
 
-        {/*Evento click*/}
-        <button onClick={ () =>{
-            console.log("Hola, soy un evento click");
-        }}>Dame click!</button>
+        <p>
+          {/*Evento click*/}
+          <button onClick={ hasdadoclick }>Dame click!</button>
+        </p>
+        
+        <p>
+          {/*Evento doble click*/}
+          <button onDoubleClick={ hasdadodobleclick }>Dame doble click!</button>
+        </p>
     </div>
   )
 }
