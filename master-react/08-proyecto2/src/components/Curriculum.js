69e69d3ff1420cd1d3478810dcbf32974346f0cf
Fredy Zarate Arango <fredyzaratearango@MacBook-Pro-de-Fredy.local>
2022-12-06 11:02:43 -0600

    Agregando datos y estilos

--- master-react/08-proyecto2/src/components/Curriculum.js
+++ master-react/08-proyecto2/src/components/Curriculum.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Curriculum = () => {
   return (
-    <div>Curriculum</div>
+    <div>
+      <h1>Curriculum</h1>
+
+      <h2>Experiencia</h2>
+        <ul>
+          <li>Desarrolaldor web - 2005 a 2022</li>
+        </ul>
+      <h2>Conocimientos</h2>
+      <h2>Educación</h2>
+      <h2>Proyectos personales</h2>
+      <h2>Reconocimientos y premios</h2>
+      <h2>Otros</h2>
+
+    </div>
   )
 }
--- master-react/08-proyecto2/src/components/Servicios.js
+++ master-react/08-proyecto2/src/components/Servicios.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export const Servicios = () => {
   return (
-    <div>Servicios</div>
+    <div className='page'>
+      <h1>Servicios</h1>
+
+      <section className='services'>
+        <article className='service'>
+          <h2>Diseño web</h2>
+          <p>Hago que tu web se vea bonita</p>
+        </article>
+
+        <article className='service'>
+          <h2>Desarrolle web</h2>
+          <p>Creo tu pagina desde cero</p>
+        </article>
+
+        <article className='service'>
+          <h2>Posicionamiento web</h2>
+          <p>Hago que tu web aparezca en Google y la vea todo mundo</p>
+        </article>
+
+      </section>
+    </div>
   )
 }
